ebdb9bc6ad58c91f047f575ed99236359235dfda
maksimmanzhai <bender23max@gmail.com>
2019-05-31 14:44:46 +0300

    Basics of Ruby

---
+++ hello-world.rb
@@ -0,0 +1,10 @@
+# Program for displaying "Hello world"
+# by Maksim Manzhai
+puts 'Hello world' # Example comment
+puts '###########'
+puts
+puts 'zzz'
+print 'Hello world'
+print
+puts 'zzz'
+puts '1#2#3#4#5#6#'
---
+++ hello-worldx3b.rb
@@ -0,0 +1,7 @@
+class This_and_that
+  def three_times
+    puts 'value'
+    puts 'value'
+    puts 'value'
+  end
+end
